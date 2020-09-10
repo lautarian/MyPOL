bdeb310241452598a5c92ab7cc364208f50d2ca6
mauroavz <mauroramiro19@gmail.com>
2020-09-10 20:09:15 -0300

    login

--- MyPol/MyPol/urls.py
+++ MyPol/MyPol/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.users import views
 
 urlpatterns = [
+    path('', views.welcome),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
     path('admin/', admin.site.urls),
 ]
