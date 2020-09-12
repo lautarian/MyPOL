3902bbaebcc97ce3b1931cda615d46a70bea2b93
marceloramirezz <marcelo_19_88@hotmail.com>
2020-09-12 19:29:52 -0300

    Cambios en las cadenas de conexion y en el archivo HTML de resultados de busqueda.

--- MyPol/MyPol/views.py
+++ MyPol/MyPol/views.py
@@ -25,7 +25,7 @@ def encontraTuPol(request):
                         'Server=mypol.database.windows.net;'
                         'Database=MyPol;'
                         'UID=Administrador;'
-                        'PWD=Info+2020')
+                        'PWD=Info+2020;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
     cursor=conn.cursor()
     cursor.execute("SELECT [nombrePrestador], Esp.nombreEspecialidad, dir.direccion, "
     " Tel.prestadorTelefono, Loc.nombreLocalidad, Prov.nombreProvincia "
@@ -62,7 +62,30 @@ def encontraTuPol(request):
     return render(request,'encontraTuPol.html',{'sqlserverconn':result, 'Localidad':result2, 'Especialidad':result3})
 
 def resultadosBusq(request):
-    return render(request, "resultadosBusq.html", {})
+    conn2=pyodbc.connect('Driver={sql server};'
+                        'Server=mypol.database.windows.net;'
+                        'Database=MyPol;'
+                        'UID=Administrador;'
+                        'PWD=Info+2020;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+    cursor2=conn2.cursor()
+    cursor2.execute("SELECT [nombrePrestador], Esp.nombreEspecialidad, dir.direccion, "
+    " Tel.prestadorTelefono, Loc.nombreLocalidad, Prov.nombreProvincia "
+    "  FROM [dbo].[Prestador] Pres "
+    " LEFT JOIN dbo.Especialidad Esp "
+    " on Esp.id_especialidad=Pres.id_especialidad "
+    " LEFT JOIN dbo.Direccion Dir "
+    " on Dir.id_prestador=Pres.id_prestador "
+    " LEFT JOIN dbo.TelefonoPrestador Tel "
+    " on tel.id_prestador=pres.id_prestador "
+    " LEFT JOIN dbo.Provincia Prov  "
+    " on Prov.id_provincia = Dir.id_provincia "
+    " LEFT JOIN  dbo.Localidad Loc "
+    " on Loc.id_localidad = Dir.id_localidad ")
+    
+
+    result=cursor2.fetchall()
+    return render(request,'resultadosBusq.html',{'sqlserverconn':result})
+    #return render(request, "resultadosBusq.html", {})
 
 def perfilPOL(request):
     return render(request, "perfilPOL.html", {})
